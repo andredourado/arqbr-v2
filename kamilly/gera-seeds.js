a642ff97d7b35bef1a6c1f385e22ac5662a3e1e0
André Dourado <andreldcastro@gmail.com>
2023-05-18 10:52:49 -0300

    feat: ajustes

--- kamilly/gera-seeds.js
+++ kamilly/gera-seeds.js
@@ -36,18 +36,18 @@ const readDocumentsCsvFile = (csvFilePath) => {
       ] = line.split(';')
 
       obj.push(`
-        ${document_code};
-        ${document_description};
-        ${quebra};
-        ${field_name_1};
-        ${field_title_1};
-        ${field_strategy_1};
-        ${field_name_2};
-        ${field_title_2};
-        ${field_strategy_2};
-        ${field_name_3};
-        ${field_title_3};
-        ${field_strategy_3}`
+        ${document_code.trim().replace('\n', '')};
+        ${document_description.trim().replace('\n', '')};
+        ${quebra.trim().replace('\n', '')};
+        ${field_name_1 === undefined ? '-' : field_name_1.trim().replace('\n', '')};
+        ${field_title_1 === undefined ? '-' : field_title_1.trim().replace('\n', '')};
+        ${field_strategy_1 === undefined ? '-' : field_strategy_1.trim().replace('\n', '')};
+        ${field_name_2 === undefined ? '-' : field_name_2.trim().replace('\n', '')};
+        ${field_title_2 === undefined ? '-' : field_title_2.trim().replace('\n', '')};
+        ${field_strategy_2 === undefined ? '-' : field_strategy_2.trim().replace('\n', '')};
+        ${field_name_3 === undefined ? '-' : field_name_3.trim().replace('\n', '')};
+        ${field_title_3 === undefined ? '-' : field_title_3.trim().replace('\n', '')};
+        ${field_strategy_3 === undefined ? '-' : field_strategy_3.trim().replace('\n', '')}`
       )
     }
 
@@ -60,9 +60,29 @@ const readDocumentsCsvFile = (csvFilePath) => {
 }
 
 //
+// ????
+//
+const fieldsToObject = async (fields) => {
+  let output = []
+
+  for await (let field of fields) {
+    const parts = field.split(';')
+
+    const tempObject = {
+      key: parts[1].trim(),
+      line: field.trim()
+    }
+
+    output.push(tempObject)
+  }
+
+  return output
+}
+
+//
 // write textfile informing documents array and file name
 //
-const writeSeedFiles = async (documents, documentsFilePath, fieldsFilePath) => {
+const writeSeedFiles = async (documents, fields, documentsFilePath, fieldsFilePath) => {
   let documentsOutput = ''
   let fieldsOutput = ''
   const departaments = {
@@ -70,6 +90,10 @@ const writeSeedFiles = async (documents, documentsFilePath, fieldsFilePath) => {
     '1025011025': '9ee9bffe-ba28-4b28-aad9-ddb15232130a',
     '1025011015': 'a82280dc-85cc-45b3-8253-69919b0df1d7'
   }
+
+  const documentFields = await fieldsToObject(fields)
+  console.log(documentFields)
+  return
 
   try {
     for await ( let document of documents) {
@@ -84,7 +108,7 @@ const writeSeedFiles = async (documents, documentsFilePath, fieldsFilePath) => {
         field_definition_3,
       ] = document.split(';')
 
-      cost_center = document_code.substr(6, 10)
+      cost_center = document_code.trim().substr(6, 10)
 
       if (cost_center.trim() !== '') {
         const splitted_field_definition_1 = field_definition_1.trim().replace('\n', '').split('|')
@@ -103,13 +127,16 @@ const writeSeedFiles = async (documents, documentsFilePath, fieldsFilePath) => {
         const field_title_3 = splitted_field_definition_3[1]
         const field_strategy_3 = splitted_field_definition_3[2]
 
-        documentsOutput += `('${documentUUID}',`
-        documentsOutput += ` '27cff971-6f84-41d8-926c-d209b30df79b',`
-        documentsOutput += ` '${departaments[cost_center]}',`
-        documentsOutput += ` '${document_description}',` 
-        documentsOutput += ` '${document_code}',` 
-        documentsOutput += ` '${quebra}',` 
-        documentsOutput += ` '', false, 'now()', 'now()'),\n` 
+        tempDocumentsOutput = ''
+        tempDocumentsOutput += `('${documentUUID}',`
+        tempDocumentsOutput += ` '27cff971-6f84-41d8-926c-d209b30df79b',`
+        tempDocumentsOutput += ` '${departaments[cost_center]}',`
+        tempDocumentsOutput += ` '${document_description.trim()}',` 
+        tempDocumentsOutput += ` '${document_code.trim()}',` 
+        tempDocumentsOutput += ` '${quebra.trim()}',` 
+        tempDocumentsOutput += ` '', false, 'now()', 'now()'),\n` 
+
+        documentsOutput += tempDocumentsOutput.replace('\n', '') + '\n'
 
         fieldsOutput += `${generateUUID()},`
         fieldsOutput += ` '${documentUUID}',`
@@ -160,6 +187,10 @@ const writeSeedFiles = async (documents, documentsFilePath, fieldsFilePath) => {
 (async () => {
   // read csv files
   const documents = await readDocumentsCsvFile('./quebra.csv')
+  const fields = await readDocumentsCsvFile('./campos.csv')
 
-  writeSeedFiles(documents, './quebra.txt', './campos.txt')
+  console.log(fields)
+  return
+
+  writeSeedFiles(documents, fields, './tipo_documento.txt', './tipo_documento_campos.txt')
 })()
